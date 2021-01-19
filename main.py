d5ebb673b58ef68815a585822bd80bbd774e69ae
Andy-Vz <carlosandresav@hotmail.com>
2021-01-19 19:59:24 +0100

    Added the first creation of the walls

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
-
 class Character(object):
     def __init__(self):
         self.charX = 9
@@ -9,12 +8,20 @@ class Character(object):
 
     def draw(self, canvas):
         # Draw Background
-        canvas.create_image(0, 0, anchor=NW, image=background)
+        canvas.create_image(0, 0,anchor=NW, image=background)
         # Draw Character
         canvas.create_image(self.charX, self.charY, anchor=NW, image=boss)
-
         # Draw the walls
-
+        initLevelMap = open("levels/level1.txt", "r")
+        lineCount = -1
+        for mapLine in initLevelMap:
+            lineCount += 1
+            positions = mapLine.strip().split(",")
+            for position in positions:
+                if position == "1" :
+                    x = positions.index(position)
+                    y = lineCount
+                    canvas.create_image(x*90 + 9, y*90 + 9, anchor=NW, image=wall)
 
 # Create the tk environment as usual
 root = Tk()
@@ -27,7 +34,6 @@ wall = ImageTk.PhotoImage(Image.open("artifacts/wall.png"))
 
 # Draw character
 char = Character()
-
 
 # Create a function that can be called when a key pressing happens
 def on_key_press(e):
@@ -42,7 +48,6 @@ def on_key_press(e):
         char.charX = char.charX - 90
     # draw the char again in the new position
     char.draw(canvas)
-
 
 # Tell the canvas that we prepared a function that can deal with the key press events
 canvas.bind("<KeyPress>", on_key_press)
